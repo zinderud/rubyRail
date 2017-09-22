e97cc590f61bc2654f7e185c4b9bfc5f932395c4
zinderud <mokosam@gmail.com>
2017-09-22 16:29:24 +0300

    arrays

--- second/4.arrys.rb
+++ second/4.arrys.rb
@@ -107,4 +107,78 @@ t.rassoc("kırmızı")   # => ["renkler", "kırmızı", "sarı", "mavi"]
 
 ["Uğur", "Yeşim", "Ezel", "Ömer"].find { |n| n.length > 3 } # => "Uğur"
 ["Uğur", "Yeşim", "Ezel", "Ömer"].find_all { |n| n.length > 3 } # => ["Uğur", "Yeşim", "Ezel", "Ömer"]
-["a", "b", "c", "d", "e"].index("e")    
+["a", "b", "c", "d", "e"].index("e")
+
+a = [1, 2, 3]
+a.clear # => []
+a       # => []
+
+a = [1, 2, 3, 4, 5]
+a.reverse # => [5, 4, 3, 2, 1]
+
+# sample Array'den random olarak eleman almaya yarar. Eğer parametre geçilirse geçilen adet kadar random eleman döner.
+
+a = [1, 2, 3, 4, 5]
+a.sample    # => 3
+
+# shuffle Array'in içindeki elemanların index'lerini karıştırı :)
+
+a = [1, 2, 3, 4, 5]
+a.shuffle # => [5, 4, 1, 3, 2]
+a.shuffle # => [1, 2, 3, 5, 4]
+a = ["Uğur", "Yeşim", "Ezel", "Ömer"]
+a.fill("x")       # => ["x", "x", "x", "x"] # tüm elemanları x yaptı
+a = [1, 2, 3, 4, 5]
+a.fill { |i| i * 5 }     # => [0, 5, 10, 15, 20]
+a                        # => [0, 5, 10, 15, 20]
+a = [1, 2, 3, 4, 5]
+
+a.rotate    # => [2, 3, 4, 5, 1] # 1 kaydırdı
+a.rotate(2) # => [3, 4, 5, 1, 2] # 2 kaydırdı, ilk 2 elemanı sona koydu!
+
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+
+[1, 2, 3].zip(a, b)   # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+[1, 2].zip(a, b)      # => [[1, 4, 7], [2, 5, 8]]
+a.zip([1, 2], [8])    # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+
+
+
+(1..10).to_a        # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 2'den 5'e kadar (5 dahil)
+(1..10).grep 2..5   # => [2, 3, 4, 5]
+
+
+p a = [1, 2, 3, 4, 5]
+p a.collect { |i| i * 2 }       # => [2, 4, 6, 8, 10]
+p a.collect { |i| "sayı #{i}" }
+
+p ["Uğur", "Yeşim", "Ezel", "Ömer"].map { |isim| "İsim: #{isim}" } # => ["İsim: Uğur", "İsim: Yeşim", "İsim: Ezel", "İsim: Ömer"]
+p ["Uğur", "Yeşim", "Ezel", "Ömer"].collect { |isim| "İsim: #{isim}" } # => ["İsim: Uğur", "İsim: Yeşim", "İsim: Ezel", "İsim: Ömer"]
+
+
+
+[1, 2, 3, 10, 15, 20].select { |n| n % 2 == 0 }
+
+
+
+p a = [1, 2, 3]
+p a.combination(1).to_a
+p a.combination(2).to_a
+p a.combination(3).to_a
+p a.permutation(1).to_a
+p a.permutation(2).to_a
+p a.permutation(3).to_a
+
+p a.cycle(2).to_a
+
+
+
+notlar = [40, 45, 53, 70, 99, 65]
+notlar.drop_while {|notu| notu < 50 }   # => [53, 70, 99, 65]
+notlar = [40, 45, 53, 70, 99, 65]
+notlar.take_while { |notu| notu < 50 } # => [40, 45]
+p a.each { |k| puts "k: #{k}" }
