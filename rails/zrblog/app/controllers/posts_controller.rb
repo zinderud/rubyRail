19513c0524f302ab48a0670d6f4a94ad7319e1fc
zinderud <mokosam@gmail.com>
2017-09-30 02:59:16 +0300

    1.1

--- rails/zrblog/app/controllers/posts_controller.rb
+++ rails/zrblog/app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :description, :slug)
+      params.require(:post).permit(:title, :body, :description)
     end
 end
